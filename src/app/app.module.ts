c4236b15deb3896693c0df669783806432602160
Bill Odom <bill@billodom.com>
2023-02-27 20:56:45 -0600

    Upgrade to Angular v15.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,23 @@ import { VideoPlayerComponent } from './video-player/video-player.component';
 import { VideoThumbnailComponent } from './video-thumbnail/video-thumbnail.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
-import { examples } from './examples';
+import { ExampleBlocksComponent       } from './example-blocks/example-blocks.component';
+import { ExampleCenteringComponent    } from './example-centering/example-centering.component';
+import { ExampleChessboardComponent   } from './example-chessboard/example-chessboard.component';
+import { ExampleDebuggingComponent    } from './example-debugging/example-debugging.component';
+import { ExampleFormsComponent        } from './example-forms/example-forms.component';
+import { ExampleFullViewportComponent } from './example-full-viewport/example-full-viewport.component';
+import { ExampleImageGalleryComponent } from './example-image-gallery/example-image-gallery.component';
+import { ExampleMediaObjectsComponent } from './example-media-objects/example-media-objects.component';
+import { ExampleOverlapComponent      } from './example-overlap/example-overlap.component';
+import { ExamplePlayingCardsComponent } from './example-playing-cards/example-playing-cards.component';
+import { ExampleSandboxComponent      } from './example-sandbox/example-sandbox.component';
+import { ExampleSpanningComponent     } from './example-spanning/example-spanning.component';
+import { ExampleTransformComponent    } from './example-transform/example-transform.component';
+import { ExampleVideoViewerComponent  } from './example-video-viewer/example-video-viewer.component';
 
 @NgModule({
   declarations: [
-
-    examples.map( example => example.component ),
-
     AppComponent,
     AboutComponent,
     CaptionedImageComponent,
@@ -33,6 +43,21 @@ import { examples } from './examples';
     VideoPlayerComponent,
     VideoThumbnailComponent,
     WelcomeComponent,
+
+    ExampleCenteringComponent,
+    ExampleSandboxComponent,
+    ExampleBlocksComponent,
+    ExampleDebuggingComponent,
+    ExampleImageGalleryComponent,
+    ExamplePlayingCardsComponent,
+    ExampleSpanningComponent,
+    ExampleOverlapComponent,
+    ExampleFormsComponent,
+    ExampleMediaObjectsComponent,
+    ExampleChessboardComponent,
+    ExampleTransformComponent,
+    ExampleFullViewportComponent,
+    ExampleVideoViewerComponent
   ],
   imports: [
     BrowserModule,
