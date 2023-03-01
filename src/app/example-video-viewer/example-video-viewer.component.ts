37baa0312d1e49b3630b29ce49b4e61786d4301f
Bill Odom <bill@billodom.com>
2023-03-01 01:28:22 -0600

    Minor code cleanup

--- src/app/example-video-viewer/example-video-viewer.component.ts
+++ src/app/example-video-viewer/example-video-viewer.component.ts
@@ -7,20 +7,18 @@ import { SpaceVideosService, SpaceVideo } from '../space-videos.service';
   selector: 'example-video-viewer',
   templateUrl: './example-video-viewer.component.html',
   styleUrls: ['./example-video-viewer.component.css'],
-//  encapsulation: ViewEncapsulation.None
 })
 export class ExampleVideoViewerComponent {
   static label = 'And Yet It Moves';
 
   currentVideo?: SpaceVideo;
-
   videosObservable: Observable<SpaceVideo[]>;
-
-  setCurrentVideo(v: SpaceVideo) {
-    this.currentVideo = v;
-  }
 
   constructor(svc: SpaceVideosService) {
     this.videosObservable = svc.load('misc');
+  }
+
+  setCurrentVideo(v: SpaceVideo) {
+    this.currentVideo = v;
   }
 }
