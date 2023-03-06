cf229f470d5df6a4b2e540757df70664d3516b3c
Bill Odom <bill@billodom.com>
2023-03-05 23:22:16 -0600

    Simplify example, feature, and menu item handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { CustomRoute, CustomRoutes } from './custom-route-types';
 
 import { AppCustomPreloader } from './app-custom-preloader';
 
-import { examples } from './examples';
-
 function setNextAndPreviousPaths(
   route: CustomRoute,
   index: number,
@@ -34,26 +32,98 @@ function setNextAndPreviousPaths(
   return newRoute;
 }
 
-const featureRoutes: CustomRoutes = [
+export const featureRoutes: CustomRoutes = [
   {
     path: 'welcome',
+    title: 'What? Who?',
     data: { preload: true },
     loadComponent: () => import('./welcome/welcome.component')
   },
   {
     path: 'menu',
+    title: 'So Many Choices',
     data: { preload: true },
     loadComponent: () => import('./main-menu/main-menu.component')
   },
 
-  ...examples,
-
+  {
+    path: 'example-centering',
+    title: 'Best Feature Ever!!1!',
+    loadComponent: () => import('./example-centering/example-centering.component')
+  },
+  {
+    path: 'example-sandbox',
+    title: 'Some Assembly Required',
+    loadComponent: () => import('./example-sandbox/example-sandbox.component')
+  },
+  {
+    path: 'example-blocks',
+    title: 'Playing With Blocks',
+    loadComponent: () => import('./example-blocks/example-blocks.component')
+  },
+  {
+    path: 'example-debugging',
+    title: 'Good Grids Gone Bad',
+    loadComponent: () => import('./example-debugging/example-debugging.component')
+  },
+  {
+    path: 'example-image-gallery',
+    title: 'Ooooh Pretty',
+    loadComponent: () => import('./example-image-gallery/example-image-gallery.component')
+  },
+  {
+    path: 'example-playing-cards',
+    title: 'Playing With a Full Deck',
+    loadComponent: () => import('./example-playing-cards/example-playing-cards.component')
+  },
+  {
+    path: 'example-spanning',
+    title: 'It\'s All Going According to Span',
+    loadComponent: () => import('./example-spanning/example-spanning.component')
+  },
+  {
+    path: 'example-overlap',
+    title: 'When Worlds Collide',
+    loadComponent: () => import('./example-overlap/example-overlap.component')
+  },
+  {
+    path: 'example-forms',
+    title: 'Plato Would be Proud',
+    loadComponent: () => import('./example-forms/example-forms.component')
+  },
+  {
+    path: 'example-media-objects',
+    title: 'I Object!',
+    loadComponent: () => import('./example-media-objects/example-media-objects.component')
+  },
+  {
+    path: 'example-chessboard',
+    title: 'The Grid of Kings',
+    loadComponent: () => import('./example-chessboard/example-chessboard.component')
+  },
+  {
+    path: 'example-transform',
+    title: 'I Want to Break Free',
+    loadComponent: () => import('./example-transform/example-transform.component')
+  },
+  {
+    path: 'example-full-viewport',
+    title: 'So Much Room for Activities!',
+    loadComponent: () => import('./example-full-viewport/example-full-viewport.component')
+  },
+  {
+    path: 'example-video-viewer',
+    title: 'And Yet It Moves',
+    loadComponent: () => import('./example-video-viewer/example-video-viewer.component')
+  },
   {
     path: 'about',
+    title: 'Say "What?" Again',
     loadComponent: () => import('./about/about.component')
   },
   {
     path: 'resources',
+    title: 'And Furthermore',
     loadComponent: () => import('./resources/resources.component')
   }
 ];
@@ -63,7 +133,7 @@ const appRoutes: CustomRoutes = [
 
   ...featureRoutes.map(setNextAndPreviousPaths),
 
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'menu' }
 ];
 
 @NgModule({
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { RouterLinkActive, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
-import { examples } from '../examples';
+import { CustomRoute } from '../custom-route-types';
+import { featureRoutes } from '../app-routing.module';
+
+function ensureTitlesAreStrings(item: CustomRoute): CustomRoute {
+  return {
+    ...item,
+    title: typeof(item.title) === 'string'
+      ? item.title
+      : item.path
+  };
+}
 
 @Component({
   selector: 'main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss'],
   standalone: true,
-  imports: [RouterLinkActive, RouterLink, NgFor]
+  imports: [RouterLink, NgFor]
 })
 export default class MainMenuComponent {
-  examples = examples;
+  menuItems = featureRoutes
+    // Don't include this menu in the menu choices
+    .filter(item => item.path !== 'menu')
+    .map(ensureTitlesAreStrings)
+  ;
 }
